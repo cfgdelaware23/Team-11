28d73b13b5f196f50c7528a0c960c4daa3b1a94f
Anirvin Vaddiyar <anirvinv@gmail.com>
2023-09-29 17:11:16 -0400

    connected mongodb to express and added routes/

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,11 @@ app.use(json({
     },
 }));
 
-app.listen(process.env.PORT, () => {
-    console.log("Ready to go");
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Ready to go");
+    });
 });
