c2e482dc2b768009ffa9d777d86a74e3b1ba5d63
Bokai-Bi <87922022+Bokai-Bi@users.noreply.github.com>
2023-09-29 19:40:44 -0400

    Updated controller for userdata

--- backend/controllers.js
+++ backend/controllers.js
@@ -4,8 +4,13 @@ export const createCustomer = (user, res) => {
     const name = user.name;
     const discount = user.discount;
     const username = user.username;
-    const lastUpdate = user.lastUpdate;
+    const currentStars = 0;
+    const lastUpdate = new Date();
     const lastUpdatedByUser = user.lastUpdatedByUser;
+
+    if (!name || !discount || !username || !lastUpdatedByUser) {
+        return res.status(400).json('User creation request lacks field')
+    }
 
     const newUser = new UserData({name, discount, username, lastUpdate, lastUpdatedByUser});
     newUser.save().then(() => {
@@ -22,3 +27,25 @@ export const getCustomer = (username, res) => {
         res.status(400).json('Error: ' + err)
     })
 } 
+
+export const updateCustomer = async (username, newUserData, res) => {
+    const user = UserData.findOne({ username: username }).then((foundUsr) => {
+        if (!foundUsr) {
+            res.status(400).json("Cannot find user " + username)
+        }
+    }).catch((err) => {
+        res.status(400).json('Error when searching customer: ' + err)
+    })
+
+    const name = newUserData.name
+    const discount = newUserData.discount
+    const currentStars = newUserData.currentStars
+    const lastUpdate = new Date()
+    const lastUpdatedByUser = newUserData.lastUpdatedByUser
+
+    if (!name || !discount || currentStars || !username || !lastUpdatedByUser) {
+        return res.status(400).json('User creation request lacks field')
+    }
+
+    
+}
