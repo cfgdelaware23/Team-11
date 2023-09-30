2a2976ee31c0055afbc29daaeac22779061807de
erwin-xyz <dcruzee@outlook.com>
2023-09-29 20:44:17 -0400

    Parse product data

--- web/src/routes/Shop.jsx
+++ web/src/routes/Shop.jsx
@@ -41,14 +41,14 @@ export default function Shop() {
                 {groceryProducts.map((product, index) => (
                     <Col>
                         <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>{product.name}</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                    {product.description}
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Text>
+                                    ${product.price}
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
