a66ec7cc6801f6641b0215a4e760a1e0e070938c
Gretta Halollari <grettahalollari@gmail.com>
2023-09-30 08:56:42 -0400

    admin table

--- web/src/routes/AdminTable.jsx
+++ web/src/routes/AdminTable.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import "./Home.css"
+
 import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function AdminTable() { 
 
-      const [userData, setUserData] = useState({});
+      const [userData, setUserData] = useState([]);
     
       useEffect(() => {
         fetch('http://localhost:3000/admin/allCustomers')
             .then(res => res.json())
-            .then(data => setUserData(data))
+            .then(data => {setUserData(data) 
+            console.log(data)})
+            
       });
 
 
@@ -23,19 +25,21 @@ return (
         <th scope="col" className="px-6 py-3">ID</th>
         <th scope="col" className="px-6 py-3">Name</th>
         <th scope="col" className="px-6 py-3">Discount</th>
-        <th scope="col" className="px-6 py-3">Price</th>
         <th scope="col" className="px-6 py-3">Username</th>
         <th scope="col" className="px-6 py-3">Stars</th>
       </tr>
     </thead>
     <tbody>
       {userData.map((item, index) => (
-        <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-          <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+           <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+           <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">{item._id}
           </td>
-          <td className="px-6 py-4">{item.id}</td>
+          
           <td className="px-6 py-4">{item.name}</td>
           <td className="px-6 py-4">{item.discount}</td>
+          <td className="px-6 py-4">{item.username}</td>
+          <td className="px-6 py-4">{item.currentStars}</td>
+          <td className="px-6 py-4"></td>
         </tr>
       ))}
     </tbody>
