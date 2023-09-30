e16cb1db80c9a5abb027706f566b257ad540f7cd
Aksel Bell <aksel.bell@duke.edu>
2023-09-29 21:51:01 -0400

    added feedback to cashier routes

--- backend/routes/cashier-routes.js
+++ backend/routes/cashier-routes.js
@@ -37,4 +37,13 @@ router.put("/update", async (req, res) => {
     return updateCustomer(req.body.user, res);
 })
 
+router.post("/addfeedback", async (req, res) => {
+    try {
+        await updateFeedback(req.body.username, req.body.feedback);
+        res.status(200).json({ message: "Feedback added/updated successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while updating feedback" });
+    }
+})
+
 export default router;
