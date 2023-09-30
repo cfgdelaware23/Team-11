cf5011b390cd486621012d5855a35c8d8a405072
Anirvin Vaddiyar <anirvinv@gmail.com>
2023-09-30 03:34:50 -0400

    fixed feedback form

--- web/src/routes/Feedback.jsx
+++ web/src/routes/Feedback.jsx
@@ -37,11 +37,12 @@ export default function Feedback(){
         <h1 className="header">Give us your feedback! </h1>
         {/* <img className="logo" src={logoImage} alt='import'></img><br/> */}
         <h1 className="questionsContainer">How nutrious would you rank our food?</h1>
-        <button className="ratingContainer" onClick={() => handleNutritionRatingClick(1)}>1</button>
-        <button className="ratingContainer" onClick={() => handleNutritionRatingClick(2)}>2</button>
-        <button className="ratingContainer" onClick={() => handleNutritionRatingClick(3)}>3</button>
-        <button className="ratingContainer" onClick={() => handleNutritionRatingClick(4)}>4</button>
-        <button className="ratingContainer" onClick={() => handleNutritionRatingClick(5)}>5</button>
+        {[...Array(5)].map((x, i)=>{
+            if (selectedNutritionRating === i+1){
+                return <button key={`b1${i}`} className="ratingContainer pressed" onClick={() => handleNutritionRatingClick(i+1)}>{i+1}</button>
+            }
+            return <button key={`b1${i}`} className="ratingContainer" onClick={() => handleNutritionRatingClick(i+1)}>{i+1}</button>
+        })}
         <h1 className="questionsContainer">How affordable are our products to you?</h1>
         <button className="rating2Container" onClick={() => handleAffordableRatingClick(1)}>1</button>
         <button className="rating2Container" onClick={() => handleAffordableRatingClick(2)}>2</button>
